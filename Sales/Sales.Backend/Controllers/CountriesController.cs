ea822f638d351d54cc49b6f938743e2dcb23311d
Efrain Trujillo <etrujillomalaver22@hotmail.com>
2023-08-20 18:14:40 -0500

    add countries controller

---
+++ Sales/Sales.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.Backend.Data;
+using Sales.Shared.Entities;
+using System.Runtime.CompilerServices;
+
+namespace Sales.Backend.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController: ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public CountriesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _dataContext.countries.ToListAsync());
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _dataContext.Add(country);
+            await _dataContext.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
