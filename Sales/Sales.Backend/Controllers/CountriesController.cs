d6f6b1fdbda8237914b1635428f4d844eb136d07
Efrain Trujillo <etrujillomalaver22@hotmail.com>
2023-08-26 10:10:59 -0500

    Add contries list component blazor

--- Sales/Sales.Backend/Controllers/CountriesController.cs
+++ Sales/Sales.Backend/Controllers/CountriesController.cs
@@ -8,12 +8,23 @@ namespace Sales.Backend.Controllers
 {
     [ApiController]
     [Route("/api/countries")]
-    public class CountriesController: ControllerBase
+    public class CountriesController : ControllerBase
     {
         private readonly DataContext _dataContext;
         public CountriesController(DataContext dataContext)
         {
             _dataContext = dataContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            Country? country = await _dataContext.countries.FirstOrDefaultAsync(c => c.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
         }
 
         [HttpGet]
@@ -30,5 +41,42 @@ namespace Sales.Backend.Controllers
             await _dataContext.SaveChangesAsync();
             return Ok(country);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(int id, Country country)
+        {
+            Country? currentCountry = await _dataContext.countries.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (currentCountry == null)
+            {
+                return NotFound();
+            }
+
+            currentCountry.Name = country.Name;
+
+            _dataContext.Update(currentCountry);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(country);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            Country? currentCountry = await _dataContext.countries.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (currentCountry == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Remove(currentCountry);
+            await _dataContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
     }
 }
